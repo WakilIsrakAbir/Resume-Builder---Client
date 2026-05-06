b9b279163375cc8df202fd368b71b7e83e19ab1f
Wakil Israk Abir <wakilisrakabir@gmail.com>
2026-05-06 11:26:31 +0600

    create hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import { } from 'react';
 import Banner from '../components/home/Banner';
+import Hero from '../components/home/Hero';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Hero></Hero>
         </div>
     );
 };
