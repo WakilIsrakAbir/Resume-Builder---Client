1d9496053ada5d2957ff9ff88e4600725b233b95
Wakil Israk Abir <wakilisrakabir@gmail.com>
2026-05-06 10:28:02 +0600

    create some needed component and file and routing those

---
+++ src/pages/ResumeBuilder.jsx
@@ -0,0 +1,11 @@
+import { } from 'react';
+
+const ResumeBuilder = () => {
+    return (
+        <div>
+            <h1>ResumeBuilder Page</h1>
+        </div>
+    );
+};
+
+export default ResumeBuilder;
